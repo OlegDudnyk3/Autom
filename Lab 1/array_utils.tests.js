aae7e2e0202a6122fd789a52574ec5f11ad9eddc
OlegDudnyk3 <132600141+OlegDudnyk3@users.noreply.github.com>
2024-05-15 14:58:33 +0300

    Add files via upload

---
+++ Lab 1/array_utils.tests.js
@@ -0,0 +1,19 @@
+const { sumArray, filterPositiveNumbers, filterNegativeNumbers } = require('./array_utils');
+
+let testArray;
+
+beforeAll(() => {
+  testArray = [1, -2, 3, -4, 5];
+});
+
+test('сума елементів масиву', () => {
+  expect(sumArray(testArray)).toBe(3);
+});
+
+test('фільтрація позитивних чисел', () => {
+  expect(filterPositiveNumbers(testArray)).toEqual([1, 3, 5]);
+});
+
+test('фільтрація негативних чисел', () => {
+  expect(filterNegativeNumbers(testArray)).toEqual([-2, -4]);
+});
