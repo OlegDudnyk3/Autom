6818ad42abdf0cff3e84369bf3d68fa3c2d8f6d8
OlegDudnyk3 <132600141+OlegDudnyk3@users.noreply.github.com>
2024-05-15 15:00:08 +0300

    Add files via upload

---
+++ Lab 6/javascript.js
@@ -0,0 +1,28 @@
+//Додавання тестів для стартової сторінки Google
+describe('Google Homepage Tests', () => {
+    it('should open Google homepage', async () => {
+      // Test to open Google homepage
+    });
+  
+    it('should have a title "Google"', async () => {
+      // Test for title "Google"
+    });
+  
+    it('should display search input field', async () => {
+      // Test for the presence of search input field
+    });
+  
+    it('should display "Google Search" button', async () => {
+      // Test for the presence of "Google Search" button
+    });
+  
+    it('should display "I am a non-existent button" (negative test)', async () => {
+      // Negative test for the presence of non-existent button
+    });
+  
+    it('should display "I am a non-existent link" (negative test)', async () => {
+      // Negative test for the presence of non-existent link
+    });
+  });
+
+  
